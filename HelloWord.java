f70e85adf28a82f3972fac243648536b503c6954
Tuoicm18 <66233758+Tuoicm18@users.noreply.github.com>
2020-10-15 10:23:31 +0700

    Add Program HelloWord (Chi them 1 lan cac ban khac khong can lam buoc nay)

--- HelloWord.java
+++ HelloWord.java
@@ -3,6 +3,5 @@ package Ex1;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Cao Dang Sai Gon");
-        System.out.println("1. Cao Minh Tuoi");
     }
 }
