c40258c8232a359a07f248ea762763b7dd013024
Tuoicm18 <66233758+Tuoicm18@users.noreply.github.com>
2020-10-15 10:32:00 +0700

    Them Sinh Vien Cao Minh Tuoi Vao HelloWord.java

--- HelloWord.java
+++ HelloWord.java
@@ -3,5 +3,6 @@ package Ex1;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Cao Dang Sai Gon");
+        System.out.println("1. Cao Minh Tuoi");
     }
 }
