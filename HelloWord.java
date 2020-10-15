35f8ccec9348f5e1e3c3410beeb2e16738a9aeaf
kyth18 <kyth18.cdsg@caodangsaigon.edu.vn>
2020-10-15 21:41:57 +0700

    Trần Hoàng Kỳ update

--- HelloWord.java
+++ HelloWord.java
@@ -6,5 +6,6 @@ public class HelloWord {
         System.out.println("1. Cao Minh Tuoi");
         System.out.println("2. Lê Chí Cường");
         System.out.println("3. Nguyễn Trường Thọ");
+	System.out.println("4. Trần Hoàng Kỳ");
     }
 }
