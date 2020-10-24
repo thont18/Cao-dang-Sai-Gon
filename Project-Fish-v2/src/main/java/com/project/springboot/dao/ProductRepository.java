0724ee35264a8e9a8b1567f0dc75b20fc06cf327
binomino000 <66292566+binomino000@users.noreply.github.com>
2020-10-24 14:29:17 +0700

    latest commit

--- Project-Fish-v2/src/main/java/com/project/springboot/dao/ProductRepository.java
+++ Project-Fish-v2/src/main/java/com/project/springboot/dao/ProductRepository.java
@@ -1,11 +1,25 @@
 package com.project.springboot.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-import com.project.springboot.entity.Product;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Integer>{
+import com.project.springboot.entity.Product;
 
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+	@Query("select pro from Product pro where pro.name like %?1%")
+	List<Product> findProductByName(String name);
+
+	@Query("select pro from ProductType pro")
+	List<Product> getData(Pageable pegeable);
+
+	@Query("select count(*) from ProductType")
+	Integer getNumberLine();
+
+	@Query("select count(pro) from ProductType pro where pro.name like %?1%")
+	Integer getNumberLineForFilter(String name);
 }
--- Project-Fish-v2/src/main/java/com/project/springboot/dao/ProductTypeRepository.java
+++ Project-Fish-v2/src/main/java/com/project/springboot/dao/ProductTypeRepository.java
@@ -10,18 +10,19 @@ import java.awt.print.Pageable;
 import java.util.List;
 
 @Repository
-public interface ProductTypeRepository extends JpaRepository<ProductType, Integer>{
-    @Query("select l from ProductType l where l.name like %?1%")
-    List<ProductType> filterByNameOrCode(String nameOrCode, Pageable pageable);
+public interface ProductTypeRepository extends JpaRepository<ProductType, Integer> {
+//    @Query("select l from ProductType l where l.name like %?1%")
+//    List<ProductType> filterByNameOrCode(String nameOrCode, Pageable pageable);
+	@Query("select proty from ProductType proty where proty.name like %?1%")
+	List<ProductType> findProductTypeByName(String name);
 
-    @Query("select l from ProductType l")
-    List<ProductType> getData(Pageable pegeable);
+	@Query("select l from ProductType l")
+	List<ProductType> getData(Pageable pegeable);
 
-    @Query("select count(*) from ProductType")
-    Integer getNumberLine();
+	@Query("select count(*) from ProductType")
+	Integer getNumberLine();
 
-    @Query("select count(l) from ProductType l where l.name like %?1%")
-    Integer getNumberLineForFilter(String name);
-
+	@Query("select count(l) from ProductType l where l.name like %?1%")
+	Integer getNumberLineForFilter(String name);
 
 }
